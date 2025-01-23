1bc89db369fa4fabbf4ce647bcb1bc33b395ed5b
Gupta Nayan <66903579+nayangupta0503@users.noreply.github.com>
2025-01-23 13:01:18 +0530

    removed sensitive information from firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,15 @@
-// src/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAlUvVYK_u6ntMRSbzR1FSNMtS1HiF7QyQ",
-    authDomain: "test-63c24.firebaseapp.com",
-    projectId: "test-63c24",
-    storageBucket: "test-63c24.appspot.com",
-    messagingSenderId: "542427163114",
-    appId: "1:542427163114:web:af85e9431126f54d63ea93",
-    measurementId: "G-8QYV8WLG6S"
-  };
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
